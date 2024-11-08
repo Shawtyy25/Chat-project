1c2d4d38134f9f3f54ab6ac00e03ed984a85c64a
shawtyy <szolnokiabi25@gmail.com>
2024-11-08 21:29:56 +0100

    server config

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+
+
+
+const PORT = process.env.PORT || 3300
+server.listen(PORT, () => {
+    console.log(`A szerver fut a http://localhost:${PORT} címen`);
+})
